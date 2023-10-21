db82029b9ec2ef49db062fbe5afa2eeeaeb7bdcc
Amarta Sarkar <amartarocks3@gmail.com>
2023-10-21 21:15:11 +0530

    Fixed small code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 let Header = () => {
     return (
         <div className="navigation">
             <Link to={'/'}>Shopping cart</Link>
-            <Link to={'/Home'}>Home</Link>
-            <Link to={'/'}>Cart Page</Link>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/cart'}>Cart Page</Link>
         </div>
     )
 }
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-// import Home from './Component/Home';
-// import SingleElement from './Component/SingleElement';
 import React from "react";
-// import ErrorPage from "./Component/ErrorPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import ProductList from './components/Productlist';
 import Cart from './components/Cart';
+import Header from './components/Header';
+
 let MainComponent= () => {
   return (
       <div>
           <Provider store={store}>
+            <Header/>
             <ProductList/>
           </Provider>
       </div>
@@ -27,14 +27,7 @@ const mainRouter = createBrowserRouter([
   {
     path: "/cart",
     element: <Cart/>,
-  },
-  // {
-  //   path: "/posts/:id",
-  //   // element: <SingleElement/>
-  // },       
-  // { 
-  //   // errorElement: <ErrorPage/>
-  // }
+  }
 ])
 let element = document.getElementById('root');
 let root = ReactDOM.createRoot(element);
